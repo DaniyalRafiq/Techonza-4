8cc17f4cc3c75cb487d585656013bf2bb8273d57
bb420f@gmail.com <bb420f@gmail.com>
2023-04-18 10:49:11 +0500

    hero section done

--- js/script.js
+++ js/script.js
@@ -10,4 +10,33 @@ $(document).ready(function () {
 $('.menu_close ').click(function () {
   $(this).parent(".nav_links").toggleClass('active');
 })
+
+var swiper = new Swiper(".hero_slider", {
+  loop: true,
+  slidesPerView: 2,
+  speed:500,
+  spaceBetween: 30,
+  
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    } , 
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    breakpoints: {
+      // When window width is >= 768px
+
+      // When window width is >= 992px
+      992: {
+        slidesPerView: 4,
+      } ,
+      768:{
+        slidesPerView : 3 ,
+      }
+  
+    }
+});
+
 })
